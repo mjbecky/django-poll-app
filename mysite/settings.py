33b6ed1c7a5a2af29a8134a5d7caf03ebeebfe1c
CoDOT <becky.margajaya@gmail.com>
2024-04-29 17:09:18 +0700

    database created

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db_polls",
+        "USER": "app_polls",
+        "PASSWORD": "polls123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
